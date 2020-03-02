5dd29983be4b9fe667f09509319ec05e3a89d5f1
bindu <sri.ts2012@gmail.com>
2020-03-02 18:04:22 +0530

    commiting from projectYdemo first time

---
+++ src/package1/test1.java
@@ -0,0 +1,10 @@
+package package1;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		System.out.println("i am printing from test1 f package 1 of projectYdemo");
+
+	}
+
+}
---
+++ src/package2/test2.java
@@ -0,0 +1,14 @@
+package package2;
+
+public class test2 {
+	
+	class A
+	{
+		public void x()
+		{
+			System.out.println("I am printing from test2 packaga2 methodx" );
+
+		}
+	}
+
+}
